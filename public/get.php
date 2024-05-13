e6fc22c59af8542383cb4e58d8c929490348baa7
masakitoshiaki <kd1327870@st.kobedenshi.ac.jp>
2024-05-13 12:35:41 +0900

    a

---
+++ public/get.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>GETメソッドのデータ</title>
+</head>
+
+<body>
+    <h4>0J03024 正木駿成</h4>
+    <p>GETメソッドで送られてきたパラメータ名dataの値は、
+        <?php
+        echo $_GET['data']  . " です。</p>";
+        ?>
+</body>
+
+</html>
